7877494374c0de4e1455c0580db0c13bddc572cd
elpunkt <25391223+elpunkt@users.noreply.github.com>
2022-08-09 19:20:42 +0200

    now really really

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -6,13 +6,13 @@ let envWSUrl = '';
 
 if (env === 'production') {
   envApiUrl = `https://${process.env.VUE_APP_DOMAIN_PROD}${process.env.VUE_APP_API_PATH}`;
-  envWSUrl = `ws://${process.env.VUE_APP_DOMAIN_PROD}/ws`;
+  envWSUrl = `wss://${process.env.VUE_APP_DOMAIN_PROD}/ws`;
 } else if (env === 'staging') {
   envApiUrl = `https://${process.env.VUE_APP_DOMAIN_STAG}${process.env.VUE_APP_API_PATH}`;
   envWSUrl = `wss://${process.env.VUE_APP_DOMAIN_STAG}/ws`;
 } else {
   envApiUrl = `http://${process.env.VUE_APP_DOMAIN_DEV}${process.env.VUE_APP_API_PATH}`;
-  envWSUrl = `wss://${process.env.VUE_APP_DOMAIN_DEV}/ws`;
+  envWSUrl = `ws://${process.env.VUE_APP_DOMAIN_DEV}/ws`;
 }
 
 export const wsUrl = envWSUrl;
