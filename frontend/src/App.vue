d27977f1174ca79fa90a2b64a6019177fc92f38a
elpunkt <25391223+elpunkt@users.noreply.github.com>
2022-08-12 14:31:45 +0200

    Added luona credits and bouqet favicon

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <template>
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💐</text></svg>">
   <Header/>
   <router-view/>
   <NotificationsManager/>
+  <div class="credits">
+    by <a href="https://luona.dev">[luona]</a> development
+  </div>
 </template>
 <script>
 import Header from '@/components/Header.vue';
@@ -22,6 +26,8 @@ export default {
 
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Abel&family=Roboto&display=swap');
+
 *{
   margin: 0;
   padding: 0;
@@ -36,13 +42,29 @@ export default {
   color: #2c3e50;
 }
 
+.credits {
+  font-size: 0.8rem;
+  background-color: black;
+  padding: 3px;
+  margin-left: 5px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  color: white;
+  a {
+    color: white;
+    text-decoration: none;
+    color: gold;
+    font-family: Abel, sans-serif;
+  }
+}
+
 #nav {
   padding: 30px;
 
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
